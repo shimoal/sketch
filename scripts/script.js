ae2b3d184ccd57a2c23c02be7eb174ca6d7fcd20
Al <shimoalmail@gmail.com>
2016-09-10 22:03:54 +0900

    added change grid size button

--- scripts/script.js
+++ scripts/script.js
@@ -1,46 +1,39 @@
 $(document).ready(function(){
-
-
 //creating the grid
-var size = 40;
+ var size = 40;
 
-/* not working yet
-$('#newSize').click(function(){
-	size = prompt("what size would you like?");
-	});
-*/
+ $('#newSize').click(function(){
+ 	$('#container').empty();
+	size = prompt("what size would you like?");//needs to check for valid input
 
-
-var dimension = (1000 / size).toString();
-for(var j = 0; j < size; j++){
-
-	for (var i = 0; i< size; i ++){
-  	$('#container').append($('<div class="box"></div>').css({'width': dimension+'px', 'height': dimension+'px'}));
+	var dimension = (1000 / size).toString();
+	for(var j = 0; j < size; j++){
+		for (var i = 0; i< size; i ++){
+  		$('#container').append($('<div class="box"></div>').css({'width': dimension+'px', 'height': dimension+'px'}));
+		};
+ 	 $('#container').append('<br>');
 	};
-  $('#container').append('<br>');
-};
-
 
 
 //functionality
-$('.box').mouseenter(function(){
-  var opacityLevel = $(this).css('opacity');
-    opacityLevel -= 0.2;
-  $(this).css({ 'opacity': opacityLevel});
-});
+  $('.box').mouseenter(function(){
+    var opacityLevel = $(this).css('opacity');
+      opacityLevel -= 0.2;
+    $(this).css({ 'opacity': opacityLevel});
+  });
 
-$('.box').click(function(){
-	$(this).css({'opacity': 1.0});
-	});
+  $('.box').click(function(){
+	 $(this).css({'opacity': 1.0});
+  });
 
 
 
 //buttons
-$('#clear').click(function(){
-	$('.box').css({'opacity': 1.0});
-	});
+  $('#clear').click(function(){
+	 $('.box').css({'opacity': 1.0});
+  });
 
-
+ });
 });
 
 
