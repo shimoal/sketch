0ce1cdbdec91a6ddc12e485d31a0c1dc9f9152bd
Al <shimoalmail@gmail.com>
2016-09-14 00:03:39 +0900

    added all functions, buttons, footer, and began styling. Worked out most of the bugs!

--- scripts/script.js
+++ scripts/script.js
@@ -1,39 +1,87 @@
 $(document).ready(function(){
-//creating the grid
- var size = 40;
 
- $('#newSize').click(function(){
- 	$('#container').empty();
-	size = prompt("what size would you like?");//needs to check for valid input
 
-	var dimension = (1000 / size).toString();
-	for(var j = 0; j < size; j++){
-		for (var i = 0; i< size; i ++){
-  		$('#container').append($('<div class="box"></div>').css({'width': dimension+'px', 'height': dimension+'px'}));
+//generate a new grid after selecting a mode
+	$('.mode-button').on('click', function(){
+			var current_mode = $(this).val();
+ 			newSize();
+			sketchMode(current_mode);
+	});
+
+//building the grid
+	function buildGrid(size){
+		var dimension = (1000 / size).toString();
+		for(var j = 0; j < size; j++){
+			for (var i = 0; i< size; i ++){
+  			$('#container').append($('<div class="box"></div>').css({'width': dimension+'px', 'height': dimension+'px'}));
+			};
+	 	 		$('#container').append('<br>');
 		};
- 	 $('#container').append('<br>');
+		$('#container').css({'width': dimension*size + 'px'});
 	};
 
+ 	function newSize(){
+ 		$('#container').empty();
+ 		var new_size = prompt("what size would you like?");//needs to check for valid input
+			buildGrid(new_size);
+ 	};
+ 	
+ 	
+//functionality for various modes
+		function sketchMode(mode){
+		switch(mode) {
+			case "black":
+				$('.box').mouseenter(function(){
+	 				$(this).addClass('black');
+	 			});
+	 			break;
+	 		case "red":
+				$('.box').mouseenter(function(){
+	 				$(this).addClass('red');
+	 			});
+	 			break;
+	 		case "blue":
+	 			$('.box').mouseenter(function(){
+	 				$(this).addClass('blue');
+	 			});
+	 			break;
+	 		case "rainbow":
+	 			$('.box').mouseenter(function() {
+	 				var a = Math.floor(Math.random() * 256);
+	 				var b = Math.floor(Math.random() * 256);
+	 				var c = Math.floor(Math.random() * 256);
+	 				$(this).css({'background-color': 'rgb('+a+','+b+','+c+')'});
+	 			});
+	 			break;
+	 		case "fade":
+	 			$('#container').addClass('black');
+	 			$('.box').addClass('white');
+	 			$('.box').mouseenter(function(){
+     				var opacityLevel = $(this).css('opacity');
+       			opacityLevel -= 0.2;
+    				 $(this).css({ 'opacity': opacityLevel});
+   			});
+   			break;
+   		case "fade-to-white":
+   			$('#container').addClass('white');
+   			$('.box').addClass('black');
+   			$('.box').mouseenter(function(){
+     				var opacityLevel = $(this).css('opacity');
+       			opacityLevel -= 0.2;
+    				 $(this).css({ 'opacity': opacityLevel});
+   			});
+	 	}
+	};
 
-//functionality
-  $('.box').mouseenter(function(){
-    var opacityLevel = $(this).css('opacity');
-      opacityLevel -= 0.2;
-    $(this).css({ 'opacity': opacityLevel});
-  });
-
-  $('.box').click(function(){
-	 $(this).css({'opacity': 1.0});
-  });
-
-
-
-//buttons
+	//buttons
   $('#clear').click(function(){
-	 $('.box').css({'opacity': 1.0});
+	 $('.box').css({'background-color': 'white', 'opacity': 1.0});
   });
 
- });
-});
+	
+	//the initial default grid
+	buildGrid(20);
+	sketchMode("black");
 
+});
 
---
+++ todo.md
@@ -0,0 +1,4 @@
+To do list:
+-make a menu for options
+-spruce up CSS
+-add a footer with info
